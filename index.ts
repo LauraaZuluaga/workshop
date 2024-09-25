8a569248e5baf919c86e7e1155db74c42531f18f
LauraaZuluaga <laurazuluaga2024@gmail.com>
2024-09-25 14:07:27 -0500

    Ejercicio 7

--- index.ts
+++ index.ts
@@ -27,3 +27,24 @@ console.log("Su edad es: ", persona1.getEdad());
 
 const empleado1 = new Empleado("Juan", 40, 500000);
 console.log(empleado1.trabajar(8));
+
+const empleado2 = new Empleado ("Anderson", 19, 50000);
+const empleado3 = new Empleado ("Camila", 29, 10000);
+const empleado4 = new Empleado ("Sara", 20, 15000);
+const empleado5 = new Empleado ("Estefania", 30, 20000);
+const empleado6 = new Empleado ("Andrea", 15, 100);
+const empleado7 = new Empleado ("Manuela", 60, 5000);
+const empleado8 = new Empleado ("Constanza", 70, 40000);
+const empleado9 = new Empleado ("Wendy", 27, 18000);
+const empleado10 = new Empleado ("Sonia", 40, 2000);
+
+console.log(empleado1.saludar());
+console.log(empleado2.saludar());
+console.log(empleado3.saludar());
+console.log(empleado4.saludar());
+console.log(empleado5.saludar());
+console.log(empleado6.saludar());
+console.log(empleado7.saludar());
+console.log(empleado8.saludar());
+console.log(empleado9.saludar());
+console.log(empleado10.saludar());
