02579875e00d1d40493ee242bf704c572376747b
LauraaZuluaga <laurazuluaga2024@gmail.com>
2024-09-25 13:37:33 -0500

    Ejercicio 2

---
+++ Persona.ts
@@ -0,0 +1,15 @@
+
+
+class Persona {
+    nombre: string;
+    edad: number;
+
+    constructor(nombre: string, edad: number) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    saludar(): string {
+        return `Persona: Su nombre es ${this.nombre} y tiene ${this.edad} años.`;
+    }
+}
