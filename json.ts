3cf277b14e6d993f4accff3053a7b6c7f20af843
LauraaZuluaga <laurazuluaga2024@gmail.com>
2024-09-28 20:51:38 -0500

    Corrección ejercicio 12

---
+++ json.ts
@@ -0,0 +1,53 @@
+
+const jsonEmpleados = [
+    {
+        "nombre": "Ana",
+        "edad": 30,
+        "salario": 3000
+    },
+    {
+        "nombre": "Luis",
+        "edad": 25,
+        "salario": 2500
+    },
+    {
+        "nombre": "María",
+        "edad": 28,
+        "salario": 3200
+    },
+    {
+        "nombre": "Carlos",
+        "edad": 35,
+        "salario": 4000
+    },
+    {
+        "nombre": "Sofía",
+        "edad": 29,
+        "salario": 2900
+    },
+    {
+        "nombre": "Pedro",
+        "edad": 40,
+        "salario": 4500
+    },
+    {
+        "nombre": "Lucía",
+        "edad": 33,
+        "salario": 3700
+    },
+    {
+        "nombre": "Jorge",
+        "edad": 27,
+        "salario": 3100
+    },
+    {
+        "nombre": "Marta",
+        "edad": 31,
+        "salario": 3400
+    },
+    {
+        "nombre": "Andrés",
+        "edad": 36,
+        "salario": 4200
+    }
+];
